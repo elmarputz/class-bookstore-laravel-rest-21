6086c6b6259ba067bb494f7232fc34c27b1905aa
Elmar Putz <ep@studiomitte.com>
2021-03-05 10:59:20 +0100

    book migration added

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $books = [
+        'Herr der Ringe',
+        'Test 123'
+    ];
+    return view('welcome', compact('books'));
 });
