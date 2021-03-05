0f2bd672cd93f88b7b095e30aef2cb283196a31d
Elmar Putz <ep@studiomitte.com>
2021-03-05 11:40:26 +0100

    book list & detail view

--- routes/web.php
+++ routes/web.php
@@ -15,10 +15,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     $books = DB::table('books')->get();
-    return view('welcome', compact('books'));
+    return view('books.index', compact('books'));
 });
 
 Route::get('/books/{id}', function ($id) {
     $book = DB::table('books')->find($id);
-    dd ($book);
+    return view('books.show', compact('book'));
 });
