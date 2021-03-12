42e38e547d429c4a287f63c5bbe2581f95530035
Elmar Putz <ep@studiomitte.com>
2021-03-12 11:00:15 +0100

    author book relation m:n

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Book extends Model
@@ -30,6 +31,13 @@ class Book extends Model
     public function images() : HasMany
     {
         return $this->hasMany(Image::class);
+    }
+
+    /**
+     * book belongs to many authors
+     */
+    public function authors() : BelongsToMany {
+        return $this->belongsToMany(Author::class)->withTimestamps();
     }
 
 
