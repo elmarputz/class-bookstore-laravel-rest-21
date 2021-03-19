c0b3b0a00af54ee2563c6742cfd372ba538d9f55
Elmar Putz <ep@studiomitte.com>
2021-03-19 10:45:03 +0100

    save book & postman test

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -4,6 +4,10 @@ namespace App\Http\Controllers;
 
 use App\Models\Book;
 use Illuminate\Http\Request;
+use App\Models\Image;
+use App\Models\Author;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\DB;
 
 class BookController extends Controller
 {
@@ -37,10 +41,65 @@ class BookController extends Controller
         return $book;
     }
 
+    public function save (Request $request) : JsonResponse {
+
+        $request = $this->parseRequest($request);
+
+        DB::beginTransaction();
+        try {
+
+            $book = Book::create($request->all());
+
+            // save images
+            if (isset($request['images']) && is_array($request['images'])) {
+                foreach ($request['images'] as $img) {
+                    $image =
+                        Image::firstOrNew(['url' => $img['url'], 'title' => $img['title']] );
+                    $book->images()->save($image);
+
+                }
+            }
+
+            // save authors
+            if (isset($request['authors']) && is_array($request['authors'])) {
+                foreach ($request['authors'] as $auth) {
+                    $author =
+                        Author::firstOrNew(['firstName' => $auth['firstName'], 'lastName' => $auth['lastName']] );
+                    $book->authors()->save($author);
+
+                }
+            }
+
+            DB::commit();
+            return response()->json($book, 201);
+        }
+
+        catch (\Exception $e) {
+
+            DB::rollBack();
+            return response()->json("saving book failed: " . $e->getMessage(), 420);
+
+        }
+
+
+    }
+
 
 
     public function show($book) {
         $book = Book::find($book);
         return view('books.show', compact('book'));
     }
+
+    /**
+     * modify / convert values if needed
+     */
+    private function parseRequest(Request $request) : Request {
+        // get date and convert it - its in ISO 8601, e.g. "2018-01-01T23:00:00.000Z"
+        $date = new \DateTime($request->published);
+        $request['published'] = $date;
+        return $request;
+    }
+
+
 }
